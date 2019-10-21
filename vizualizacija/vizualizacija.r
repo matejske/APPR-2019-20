48d023efda7611304c52e96569f7d8dbbd69fb91
matejske <matej.skerlep@gmail.com>
2019-10-21 18:54:21 +0200

    Manjsi popravki

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -23,31 +23,34 @@ graf.davki.slo <- ggplot(davki.slovenije, aes(x = Leto, y = Pobrani.davki)) +
 
 
 
-# Uvozimo zemljevid Sveta=======================================================================
+# Uvozimo zemljevid sveta=======================================================================
 source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
 source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
 
 svet <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip", 
                         "ne_50m_admin_0_countries", encoding = "utf-8") %>% fortify()
 
-zace <- ggplot(skupno.plini, aes(x=Leto, y=skupne.emisije, col=Drzava)) + geom_point() + geom_line()
+#zace <- ggplot(skupno.plini, aes(x=Leto, y=skupne.emisije, col=Drzava)) + geom_point() + geom_line()
 
 
 # Zemljevid sveta skrčimo na zemljevid Evrope
-europe <- filter(svet, CONTINENT == "Europe")
-europe <- filter(europe, long < 55 & long > -35 & lat > 30 & lat < 75)
+europe <- svet %>% filter(CONTINENT == "Europe") %>% 
+  filter(long < 55 & long > -35 & lat > 30 & lat < 75) %>%
+  filter(NAME != "Jersey") %>%
+  filter(NAME != "Russia")
 
-europe <- filter(europe, NAME != "Jersey")
-europe <- filter(europe, NAME != "Russia")
 colnames(europe)[26] <- 'Drzava'
-# Drzave v zemljevidu Evrope
+
+# Drzave v zemljevidu europe
 drzave <- sort(unique(europe$NAME)) 
 drzave <- as.data.frame(drzave, stringsAsFactors=FALSE) 
 names(drzave) <- "Drzava"
 
 test <-ggplot() + geom_polygon(data = europe, aes(x=long, y=lat, group=group))
+print(test)
 
-#DODAJ JANOS
+
+#DODAJ info iz issuea
 
 #FUNKCIJA ZA SHINY
 
