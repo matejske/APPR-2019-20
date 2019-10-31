f7b6ce25c20cc8e9de7f282d0aca7ceb5bb7ff37
matejske <matej.skerlep@gmail.com>
2019-10-31 15:57:19 +0100

    html

--- uvoz/uvoz_html.r
+++ uvoz/uvoz_html.r
@@ -3,7 +3,7 @@
 uvozi.bruto.domaci.proizvod <- function(){
   link <- "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"
   stran <- html_session(link) %>% read_html()
-  bruto.domaci.proizvod <- stran %>% html_nodes(xpath= "//*[@id=\"mw-content-text\"]/div/table[2]") %>% 
+  bruto.domaci.proizvod <- stran %>% html_nodes(xpath= "//*[@id="mw-content-text"]/div/table[2]") %>% 
     html_table()
 
 }
